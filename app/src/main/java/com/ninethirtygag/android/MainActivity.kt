27e0f29948a7f144574f96712289b03029e30219
enigma <ninadthakare458@gmail.com>
2021-10-02 21:06:33 +0530

    Setting up dependencies

--- app/src/main/java/com/ninethirtygag/android/MainActivity.kt
+++ app/src/main/java/com/ninethirtygag/android/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.ninethirtygag.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/ninethirtygag/android/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.ninethirtygag.android
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel:ViewModel() {
+}
