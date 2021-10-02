46d7e9ec56766493b492187da5fc6e1039e2d985
enigma <ninadthakare458@gmail.com>
2021-10-02 22:09:19 +0530

    request memes from service in viewmodel

--- app/src/main/java/com/ninethirtygag/android/MainViewModel.kt
+++ app/src/main/java/com/ninethirtygag/android/MainViewModel.kt
@@ -1,6 +1,21 @@
 package com.ninethirtygag.android
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ninethirtygag.android.data.MemeService
+import kotlinx.coroutines.launch
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
+
+    private val memeService by lazy { MemeService.create() }
+
+    init {
+        getMemes()
+    }
+
+    fun getMemes() {
+        viewModelScope.launch {
+            val memes = memeService.getMemes().data?.memes
+        }
+    }
 }
